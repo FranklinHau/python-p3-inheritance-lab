226598a68f84937ee5aa309008dc734662f42fdd
Franklin Hau <fhauchi@gmail.com>
2023-07-23 02:40:58 -0600

    inheritance lab

--- lib/student.py
+++ lib/student.py
@@ -3,6 +3,19 @@
 from user import User
 
 class Student(User):
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)
+        self.knowledge=[]
     
-    def learn(self):
-        pass
+    def learn(self, information):
+        self.knowledge.append(information)
+
+# Creating an instance of the Student class
+# my_student = Student("John", "Doe")
+
+# Learning new information
+# my_student.learn("Python is a programming language.")
+# my_student.learn("Math is fun!")
+
+# Accessing the knowledge list
+# print(my_student.knowledge)  # Output: ['Python is a programming language.', 'Math is fun!']
--- lib/teacher.py
+++ lib/teacher.py
@@ -1,10 +1,33 @@
 #!/usr/bin/env python
 
 from user import User
-
 import random
 
+knowledge = [
+    "str is a data type in Python",
+    "programming is hard, but it's worth it",
+    "JavaScript async web request",
+    "Python function call definition",
+    "object-oriented teacher instance",
+    "programming computers hacking learning terminal",
+    "pipenv install pipenv shell",
+    "pytest -x flag to fail fast",
+]
 class Teacher(User):
 
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)
+        self.knowledge=knowledge
+
     def teach(self):
-        pass
+        # we can add import random inside the method
+        return random.choice(self.knowledge)
+    
+# Create an instance of the Teacher class
+# my_teacher = Teacher("John", "Doe")
+
+# Get a random topic that the teacher can teach
+# taught_topic = my_teacher.teach()
+
+# Output the result
+# print(f"{my_teacher._first_name} {my_teacher._last_name} can teach: {taught_topic}")
